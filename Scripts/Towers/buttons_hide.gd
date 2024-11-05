0f396d2c0527bca9a6f51cb42234101fd99c348e
Ukatzz <kmike.4work@gmail.com>
2024-11-06 00:53:35 +0500

    new

--- Scripts/Towers/buttons_hide.gd
+++
@@ -1,8 +0,0 @@
-extends Panel
-
-
-func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventMouseButton and event.pressed:
-		# Проверяем, был ли щелчок за пределами панели
-		if not get_global_rect().has_point(event.position):
-			self.hide()
